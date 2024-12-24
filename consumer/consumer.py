98f895cbfe71ba5f5745ea746b384ac094c0544e
Lucas64000 <loustaletl@cy-tech.fr>
2024-12-24 16:21:25 +0100

    broker consumer etc

---
+++ consumer/consumer.py
@@ -0,0 +1,29 @@
+import sys
+import json 
+
+if sys.version_info >= (3, 12, 0):
+    import six
+    sys.modules['kafka.vendor.six.moves'] = six.moves
+
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+    'gps',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True
+)
+
+print("En attente des messages...")
+for message in consumer:
+    # # Vérifie si le message a une valeur non nulle
+    # if message.value is not None:
+    #     try:
+    #         json_data = json.loads(message.value.decode('utf-8'))
+    #         id, lat, long = json_data.values()
+    #         print(id, lat, long)
+    #     except UnicodeDecodeError as e:
+    #         print(f"Erreur de décodage pour le message : {e}")
+    # else:
+    #     print("Received an empty message. Skipping...")
+    print(message.value.decode('utf-8'))
